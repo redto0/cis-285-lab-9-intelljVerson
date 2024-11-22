88440ac629c0c4863da17ec25b2e6952163c214a
redto0 <aboccacc@umich.edu>
2024-11-22 02:01:10 -0500

    removing unnecessary boiler plate

--- Largest/src/largest/Largest.java
+++ Largest/src/largest/Largest.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package largest;
 
-/**
- *
- * @author Alex Boccaccio
- */
 public class Largest {
 
     public Largest() {
